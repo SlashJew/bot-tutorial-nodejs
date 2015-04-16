4453e47914a0968ec7c27c8b6be93dc948264c60
SlashJew <SlashJew@gmail.com>
2015-04-16 00:35:00 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,15 +5,69 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\/cool guy$/; botRegexJew = /^\/jew/;
-
+      botRegex = /^\/cool guy/;  botRegexDL = /^\/DDL/i;botRegexSalt = /^\/salt/;botRegexBan = /^\/ban/
+      botRegexAd=/^\/advance/;botRegexShrug = /^\/shrug/; botRegexSC = /^\/SDL/i;
+      botRegexP = /^\/PDL/i;  botRegexTw = /^\/twitch/i; botRegexSb = /^\/sub/; botRegexLab = /^\/lab/;
+      botRegexJew = /^\/jew/
+  var teamAb = ["NE","NO","ARI","PHI","CLE","TEN","OAK","DAL","IND","SEA","CIN","PIT","JAC"
+                ,"BAL","SD","DEN","MIN","ATL","KC","NYG","GB","DET","HOU","STL","CHI","CAR",
+                "MIA","BUF","SF","WAS","NYJ","TB"]
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
-    postMessage();
+    postMessage(cool());
     this.res.end();
-  } else {
-    console.log("don't care");
+  } 
+  else if(request.text && botRegexDL.test(request.text)) {
     this.res.writeHead(200);
+    postMessage("http://daddyleagues.com/ma32/team/"+request.text.substring(5,8)+"/depthchart");
+    this.res.end();
+  } 
+  else if(request.text && botRegexSalt.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://i.imgur.com/q3ohLtD.gif");
+    this.res.end();
+  } 
+  else if(request.text && botRegexAd.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://www.kno.com/images/store/knoadvance/overview.png");
+    this.res.end();
+  }
+  else if(request.text && botRegexBan.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://www.returnofkings.com/wp-content/uploads/2014/03/ban.jpg");
+    this.res.end();
+  } 
+  else if(request.text && botRegexShrug.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("¯\\\_(ツ)_/¯");
+    this.res.end();
+  } 
+  else if(request.text && botRegexSC.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://daddyleagues.com/ma32/team/"+request.text.substring(5,8)+"/schedule");
+    this.res.end();
+  }
+  else if(request.text && botRegexP.test(request.text)) {
+    this.res.writeHead(200);
+    var req = request.text.substring(5,request.text.length);
+    var rep = req.replace(/ /,"+");
+    postMessage("http://daddyleagues.com/ma32/players?name="+rep+"&position=all&team=all");
+    this.res.end();
+  } 
+
+  else if(request.text && botRegexTw.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://www.twitch.tv/"+request.text.substring(8,request.text.length));
+    this.res.end();
+  } 
+  else if(request.text && botRegexSb.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://www.reddit.com/r/maddenall32");
+    this.res.end();
+  } 
+  else if(request.text && botRegexLab.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://www.cornelllab.com/images/70.jpg");
     this.res.end();
   } 
   else if(request.text && botRegexJew.test(request.text)) {
@@ -21,12 +75,17 @@ function respond() {
     postMessage("http://i.groupme.com/307x362.gif.357292f527ad49a38da4e13201fbfa77.large");
     this.res.end();
   } 
+  else {
+    console.log("don't care");
+    this.res.writeHead(200);
+    this.res.end();
+  }
 }
 
-function postMessage() {
-  var botResponse, options, body, botReq;
+function postMessage(response) {
+  var botResponse,options, body, botReq;
 
-  botResponse = cool();
+  botResponse = response
 
   options = {
     hostname: 'api.groupme.com',
@@ -56,6 +115,10 @@ function postMessage() {
     console.log('timeout posting message '  + JSON.stringify(err));
   });
   botReq.end(JSON.stringify(body));
+}
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
 }
 
 
